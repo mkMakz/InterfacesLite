6a285bb82c3c190b160c19077d1b4f00a8aa06b1
mkMakz <33726337+mkMakz@users.noreply.github.com>
2018-05-20 17:09:21 +0600

    Add project files.

---
+++ InterfacesLite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student st = new Student("Maxim", "Maxim Ostroukhov");
+            st.GetName();
+            st.GetFullName();
+            st.GetAvgGrade();
+            Console.WriteLine("\n");
+            st.PrintInfo();
+
+
+        }
+    }
+}
---
+++ InterfacesLite/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesLite
+{
+    public class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get ; set; }
+        public int[] Grades { get; set; }
+        
+
+        private Random r = new Random();
+
+        public Student(string name, string fullname)
+        {
+            Name = name;
+            FullName = fullname;
+            Grades = new int[r.Next(1, 10)];
+
+            for (int i = 0; i < Grades.Length; i++)
+            {
+                Grades[i] = r.Next(1, 6);
+            }
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public double GetAvgGrade()
+        {
+            int cnt = 0;
+            for (int i = 0; i < Grades.Length; i++)
+            {
+                cnt += Grades[i];
+            }
+            return (double)(cnt / Grades.Length);
+        }
+
+        public void PrintInfo()
+        {
+
+            Console.WriteLine($"Name {GetName()}\n FullName {GetFullName()}\n Average Grade {GetAvgGrade()} ");
+
+        }
+
+    }
+}
